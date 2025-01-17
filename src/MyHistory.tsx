82ad3b858fcd40703f71f8fb55186445f15df6a8
Jacek programmer <jacek301@gmail.com>
2025-01-17 02:04:41 +0100

    centering content of container.

---
+++ src/MyHistory.tsx
@@ -0,0 +1,18 @@
+
+import { Container } from 'react-bootstrap';
+import './MyHistory.css'
+
+const MyHistory = () => {
+  return (
+    <Container className="full-screen">
+
+      <div>
+        <h1>Tutaj jest moja historia wpisów</h1>
+      </div>
+
+    </Container>
+  );
+
+};
+
+export default MyHistory
